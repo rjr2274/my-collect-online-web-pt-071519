64569d56260000011cc1da77bd469b6c5951c68e
RJ Rosa <ronaldjavierrosa2274@gmail.com>
2019-07-27 15:26:21 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 def my_collect(array)
   i = 0
-  collection = []
+  collection_new = []
   while i > array.length
-    collection << yield(array[i])
+    collection_new << yield(array[i])
     i += 1
   end
-  collection
+  collection_new
 end
